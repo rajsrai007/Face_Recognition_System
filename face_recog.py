633044d768a359d3c6bb8c05300337f9107b1047
rajsrai007 <rajshekharr222@gmail.com>
2021-07-23 10:38:47 +0530

    initial commit

---
+++ face_recog.py
@@ -0,0 +1,119 @@
+import cv2
+import numpy as np
+import os
+
+
+
+########## KNN CODE ############
+def distance(v1, v2):
+	# Eucledian 
+	return np.sqrt(((v1-v2)**2).sum())
+
+def knn(train, test, k=5):
+	dist = []
+	
+	for i in range(train.shape[0]):
+		# Get the vector and label
+		ix = train[i, :-1]
+		iy = train[i, -1]
+		# Compute the distance from test point
+		d = distance(test, ix)
+		dist.append([d, iy])
+	# Sort based on distance and get top k
+	dk = sorted(dist, key=lambda x: x[0])[:k]
+	# Retrieve only the labels
+	labels = np.array(dk)[:, -1]
+	
+	# Get frequencies of each label
+	output = np.unique(labels, return_counts=True)
+	# Find max frequency and corresponding label
+	index = np.argmax(output[1])
+	return output[0][index]
+################################
+##Initialising camera 
+cap = cv2.VideoCapture(0)
+## Face Detection
+face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_alt.xml")
+skip =0
+
+dataset_path = './data/'
+face_section = np.zeros((100,100,3))
+face_data = []
+labels=[]
+
+class_id = 0  #Labels for the given file    
+names = {}  #Mapping betweem id and name
+
+for fx in os.listdir(dataset_path):
+    if fx.endswith('.npy'):
+        #create a mapping between class id and name
+        names[class_id] = fx[:-4]
+        print("loaded" + fx)
+        data_item = np.load(dataset_path+fx)
+        face_data.append(data_item)
+
+        target = class_id*np.ones((data_item.shape[0]))
+        class_id += 1
+        labels.append(target)
+
+face_dataset  = np.concatenate(face_data,axis=0)
+face_labels = np.concatenate(labels, axis=0).reshape((-1,1))
+
+print(face_dataset.shape)
+print(face_labels.shape)
+
+trainset = np.concatenate((face_dataset,face_labels),axis=1)
+print(trainset.shape)
+# file_name = input("Enter the name of the person")
+while True:
+
+    ret,frame = cap.read()
+
+    if ret==False:
+         continue
+    
+    # gray_frame  =cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = face_cascade.detectMultiScale(frame,1.3,5)
+    faces =  sorted(faces,key=lambda f:f[2]*f[3],reverse=True)
+    for face in faces:
+        x,y,w,h=face
+         
+        #Get the face region of intrest
+        offset = 10
+        face_section = frame[y-offset:y+h+offset,x-offset:x+w+offset]
+        face_section = cv2.resize(face_section,(100,100))
+        #Predicted label output
+        out = knn(trainset,face_section.flatten())
+
+        pred_name = names[int(out)]
+        cv2.putText(frame,pred_name,(x,y-10),cv2.FONT_HERSHEY_SIMPLEX,1,(255,0,0),2,cv2.LINE_AA)
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,200),2)
+
+#         skip+=1
+#         if skip%10==0:
+#             face_data.append(face_section)
+#             print(len(face_data))
+
+    cv2.imshow("Frame",frame)
+#     cv2.imshow("Face section",face_section)
+
+    
+
+
+    key_pressed =cv2.waitKey(1)
+    if key_pressed == ord('q'):
+        break
+
+
+# face_data = np.asarray(face_data)
+# face_data = face_data.reshape((face_data.shape[0],-1))
+# print(face_data.shape)
+
+# #saving the data to file
+# np.save(dataset_path+file_name+'.npy',face_data)
+# print("Data Successfully saved at"+dataset_path+file_name+'.npy')
+
+
+cap.release()
+cv2.destroyAllWindows()
